d94e8b47d9e535e62e1c90c59ce2ac1632f87989
Pasly Ch <inwoo920621@gmail.com>
2022-08-20 01:21:15 +0900

    Initial commit

---
+++ config/webpack.common.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+// const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+dotenv.config();
+
+module.exports = {
+  entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${path.resolve(__dirname, '../public')}/index.html`,
+    }),
+    new webpack.ProvidePlugin({
+      React: 'react',
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../src/'),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+  },
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Webpack Boilerplate with React & TS
+
+Webpack v5를 통해서 React + TS 환경설정을 진행했습니다.
+
+패키지 매니저로는 yarn berry( zero install )을 사용했습니다.
+
+airbnb typescript를 적용했으며 prettier가 설정되어 있습니다.
+
+css in js 라이브러리는 포함되어 있지 않습니다.
